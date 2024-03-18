0b66bae6efbc39a03a2e786b5465535ca9680e4a
Anirudh Srivathsavan S <anirudhsrivathsavan@gmail.com>
2024-03-18 22:18:22 +0530

    sprint-2 client requirements

--- code/includes/header.php
+++ code/includes/header.php
@@ -55,23 +55,76 @@ if(isset($_SESSION['user']))
 
 
 <script src="https://kit.fontawesome.com/31b4dd131e.js" crossorigin="anonymous"></script>
+<style>
+    .mob-button-group {
+    padding-top: 3rem;
+    display: flex;
+    align-items: center;
+}
 
+.mob-button {
+    flex: 1;
+    text-align: center;
+}
+
+.mob-button a {
+    display: block;
+    background-color: rgba(0, 0, 0, 0.6);
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    transition: background-color 0.3s;
+}
+
+.mob-button a:hover {
+    background-color: rgba(0, 0, 0, 0.8);
+}
+
+.button-group .button{
+    padding: 0.5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(50, 50, 50, 0.7); /* Translucent dark grey background */
+    color: #fff;
+    text-align: center;
+    margin-bottom: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+}
+
+.button:hover {
+    background-color: rgba(255, 105, 180, 0.7); /* Darker translucent dark grey background on hover */
+}
+
+
+</style>
 
 
 
 <div class="banner">
     <div class="fluid-container">
         <div class="row">
-            <div class="col-md  d-none d-md-block">
+            <div class="col-md-10  d-none d-md-block">
                 <img class="bannerimg" src="assets\logo-icon-removebg-preview.png">
                 <img class="bannerimg" src="assets\logo-title-removebg-preview.png">
             </div>
+            <div class="col-md-2 d-none d-md-block button-group">
+                <div class="row button">Donate now</div>
+                <div class="row button">Pets Insurance</div>
+            </div>
+            
         </div>
     
         <!-- Icon for mobile screens -->
         <div class="mobile text-center d-md-none">
             <img class="bannerimg-mobile" src="assets\logo-icon-removebg-preview.png">
             <img class="bannerimg-mobile" src="assets\logo-title-removebg-preview.png">
+            <div class="row mob-button-group" style = "">
+                <div class="col mob-button"><a href="">Donate now</a></div>
+                <div class="col mob-button"><a href="">Pets Insurance</a></div>
+            </div>
         </div>
 
     </div>
@@ -79,7 +132,7 @@ if(isset($_SESSION['user']))
 
 <style>
     .banner {
-        padding: 2rem 2rem;
+        padding: 2rem 2rem ;
         background-image: url('assets/temp-background.jpg');
         background-size: cover;
         background-position: center;
